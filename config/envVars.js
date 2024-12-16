ec30e04057af6518e50586a79b3b5a4df1e4ee2b
Neko <102128502+kuroineko-06@users.noreply.github.com>
2024-12-16 08:10:14 +0700

    commit

---
+++ config/envVars.js
@@ -0,0 +1,8 @@
+export const ENV_VARS = {
+  MONGO_URI:
+    "mongodb+srv://huy12060301:hoilamdeogi@cluster0.if7ov.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  PORT: 3000,
+  JWT_SECRET: process.env.JWT_SECRET,
+  NODE_ENV: "development",
+  TMDB_API_KEY: process.env.TMDB_API_KEY,
+};
